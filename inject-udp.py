aa1f3794ecb855fb41ba73b385ec9bd4644c948c
mbridak <michael.bridak@gmail.com>
2021-03-23 14:45:48 -0700

    Changed adif message to one actually sent by Gridtracker. Should have tried that awhile ago..

--- inject-udp.py
+++ inject-udp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import socket
 
-msgFromClient       = "<command:3>Log <parameters:146> <Band:3>20M <Call:5>M4HXM <Freq:6>14.076 <Mode:3>FT8 <QSO_DATE:8>20210626 <TIME_ON:6>184000 <RST_Rcvd:3>-03 <RST_Sent:3>-07 <TX_PWR:3>5.0 <STX:6>1B ORG <SRX:6>1D SJV <GRIDSQUARE:6>DM13at <EOR>"
+msgFromClient       = "<CALL:7>W1TEST3 <GRIDSQUARE:6>DM13AT <MODE:3>FT8 <RST_SENT:0> <RST_RCVD:0> <QSO_DATE:8>20210323 <TIME_ON:6>194651 <QSO_DATE_OFF:8>20210323 <TIME_OFF:6>194651 <BAND:3>20m <FREQ:9>14.075500 <STATION_CALLSIGN:0> <MY_GRIDSQUARE:0> <CONTEST_ID:14>ARRL-FIELD-DAY <SRX_STRING:6>3A NFL <CLASS:2>3A <ARRL_SECT:3>NFL <DXCC:3>291 <COUNTRY:13>United States <EOR>"
 bytesToSend         = str.encode(msgFromClient)
 serverAddressPort   = ("127.0.0.1", 2333)
 bufferSize          = 1024
