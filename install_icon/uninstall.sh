b495fda6fa44f8cbb58826124b9c8f97dfe307dd
mbridak <michael.bridak@gmail.com>
2022-04-14 10:13:12 -0700

    removes binary in .local/bin

--- install_icon/uninstall.sh
+++ install_icon/uninstall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [ -f "~/local/bin/fielddaylogger" ]; then
+	rm ~/.local/bin/fielddaylogger
+fi
+
 xdg-icon-resource uninstall --size 64 k6gte-FieldDay
 
 xdg-desktop-icon uninstall k6gte-FieldDay.desktop
