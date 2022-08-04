a4d3f720ef964479763590c6dd53fd01ac9bb096
mbridak <michael.bridak@gmail.com>
2022-08-04 09:20:51 -0700

    initial server stuff.

---
+++ lib/server_database.py
@@ -0,0 +1,272 @@
+"""Database class to store contacts"""
+import logging
+import sqlite3
+
+
+class DataBase:
+    """Database class for our database."""
+
+    def __init__(self, database):
+        """initializes DataBase instance"""
+        self.database = database
+        self.create_db()
+
+    def create_db(self) -> None:
+        """
+        create database tables contacts if they do not exist.
+        """
+        try:
+            with sqlite3.connect(self.database) as conn:
+                cursor = conn.cursor()
+                sql_table = (
+                    "CREATE TABLE IF NOT EXISTS contacts "
+                    "(id INTEGER PRIMARY KEY, "
+                    "unique_id text NOT NULL, "
+                    "callsign text NOT NULL, "
+                    "class text NOT NULL, "
+                    "section text NOT NULL, "
+                    "date_time text NOT NULL, "
+                    "frequency INTEGER DEFAULT 0, "
+                    "band text NOT NULL, "
+                    "mode text NOT NULL, "
+                    "power INTEGER NOT NULL, "
+                    "grid text NOT NULL, "
+                    "opname text NOT NULL, "
+                    "station text NOT NULL);"
+                )
+                cursor.execute(sql_table)
+                conn.commit()
+        except sqlite3.Error as exception:
+            logging.critical("create_db: Unable to create database: %s", exception)
+
+    def log_contact(self, logme: tuple) -> None:
+        """
+        Inserts a contact into the db.
+        pass in (unique_id, hiscall, hisclass, hissection, band, mode, int(power), grid, name)
+        """
+        try:
+            with sqlite3.connect(self.database) as conn:
+                sql = (
+                    "INSERT INTO contacts"
+                    "(unique_id, callsign, class, section, date_time, frequency, "
+                    "band, mode, power, grid, opname, station) "
+                    "VALUES(?,?,?,?,datetime('now'),?,?,?,?,?,?,?)"
+                )
+                cur = conn.cursor()
+                cur.execute(sql, logme)
+                conn.commit()
+        except sqlite3.Error as exception:
+            logging.debug("DataBase log_contact: %s", exception)
+
+    def delete_contact(self, unique_id) -> None:
+        """Deletes a contact from the db by passing in UUID."""
+        if unique_id:
+            try:
+                with sqlite3.connect(self.database) as conn:
+                    sql = f"delete from contacts where unique_id='{unique_id}'"
+                    cur = conn.cursor()
+                    cur.execute(sql)
+                    conn.commit()
+            except sqlite3.Error as exception:
+                logging.debug("DataBase delete_contact: %s", exception)
+
+    def change_contact(self, qso):
+        """Update an existing contact."""
+        print(qso)
+        try:
+            with sqlite3.connect(self.database) as conn:
+                sql = (
+                    f"update contacts set callsign = '{qso[0]}', class = '{qso[1]}', "
+                    f"section = '{qso[2]}', date_time = '{qso[3]}', band = '{qso[4]}', "
+                    f"mode = '{qso[5]}', power = '{qso[6]}', station = '{qso[7]}' "
+                    f"where unique_id='{qso[8]}'"
+                )
+                print(sql)
+                cur = conn.cursor()
+                cur.execute(sql)
+                conn.commit()
+        except sqlite3.Error as exception:
+            logging.debug("DataBase change_contact: %s", exception)
+
+    def stats(self) -> tuple:
+        """
+        returns a tuple with some stats:
+        cwcontacts, phonecontacts, digitalcontacts, bandmodemult, last15, lasthour, hignpower, qrp
+        """
+        with sqlite3.connect(self.database) as conn:
+            cursor = conn.cursor()
+            cursor.execute("select count(*) from contacts where mode = 'CW'")
+            cwcontacts = str(cursor.fetchone()[0])
+            cursor.execute("select count(*) from contacts where mode = 'PH'")
+            phonecontacts = str(cursor.fetchone()[0])
+            cursor.execute("select count(*) from contacts where mode = 'DI'")
+            digitalcontacts = str(cursor.fetchone()[0])
+            cursor.execute("select distinct band, mode from contacts")
+            bandmodemult = len(cursor.fetchall())
+            cursor.execute(
+                "SELECT count(*) FROM contacts "
+                "where datetime(date_time) >=datetime('now', '-15 Minutes')"
+            )
+            last15 = str(cursor.fetchone()[0])
+            cursor.execute(
+                "SELECT count(*) FROM contacts "
+                "where datetime(date_time) >=datetime('now', '-1 Hours')"
+            )
+            lasthour = str(cursor.fetchone()[0])
+            cursor.execute(
+                "select count(*) as qrpc from contacts where mode = 'CW' and power > 5"
+            )
+            log = cursor.fetchall()
+            qrpc = list(log[0])[0]
+            cursor.execute(
+                "select count(*) as qrpp from contacts where mode = 'PH' and power > 10"
+            )
+            log = cursor.fetchall()
+            qrpp = list(log[0])[0]
+            cursor.execute(
+                "select count(*) as qrpd from contacts where mode = 'DI' and power > 10"
+            )
+            log = cursor.fetchall()
+            qrpd = list(log[0])[0]
+            cursor.execute(
+                "select count(*) as highpower from contacts where power > 100"
+            )
+            log = cursor.fetchall()
+            highpower = bool(list(log[0])[0])
+            qrp = not qrpc + qrpp + qrpd
+
+            return (
+                cwcontacts,
+                phonecontacts,
+                digitalcontacts,
+                bandmodemult,
+                last15,
+                lasthour,
+                highpower,
+                qrp,
+            )
+
+    def contacts_under_101watts(self) -> tuple:
+        """return contact tallies for contacts made below 101 watts."""
+        try:
+            with sqlite3.connect(self.database) as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    "select count(*) as cw from contacts where mode = 'CW' and power < 101"
+                )
+                c_dubs = str(cursor.fetchone()[0])
+                cursor.execute(
+                    "select count(*) as ph from contacts where mode = 'PH' and power < 101"
+                )
+                phone = str(cursor.fetchone()[0])
+                cursor.execute(
+                    "select count(*) as di from contacts where mode = 'DI' and power < 101"
+                )
+                digital = str(cursor.fetchone()[0])
+        except sqlite3.Error as exception:
+            logging.critical("DB-contacts_under_101watts: %s", exception)
+            return 0, 0, 0
+        return c_dubs, phone, digital
+
+    def qrp_check(self) -> tuple:
+        """check to see if all contacts were QRP"""
+        try:
+            with sqlite3.connect(self.database) as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    "select count(*) as qrpc from contacts where mode = 'CW' and power > 5"
+                )
+                log = cursor.fetchall()
+                qrpc = list(log[0])[0]
+                cursor.execute(
+                    "select count(*) as qrpp from contacts where mode = 'PH' and power > 10"
+                )
+                log = cursor.fetchall()
+                qrpp = list(log[0])[0]
+                cursor.execute(
+                    "select count(*) as qrpd from contacts where mode = 'DI' and power > 10"
+                )
+                log = cursor.fetchall()
+                qrpd = list(log[0])[0]
+                cursor.execute(
+                    "select count(*) as highpower from contacts where power > 100"
+                )
+                log = cursor.fetchall()
+                highpower = bool(list(log[0])[0])
+                qrp = not qrpc + qrpp + qrpd
+        except sqlite3.Error as exception:
+            logging.critical("qrpcheck: %s", exception)
+            return 0, 0
+        return qrp, highpower
+
+    def get_band_mode_tally(self, band, mode):
+        """
+        returns the amount of contacts and the maximum power used
+        for a given band using a particular mode.
+        """
+        with sqlite3.connect(self.database) as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                "select count(*) as tally, MAX(power) as mpow from contacts "
+                f"where band = '{band}' AND mode ='{mode}'"
+            )
+            return cursor.fetchone()
+
+    def get_bands(self) -> tuple:
+        """returns a list of bands"""
+        with sqlite3.connect(self.database) as conn:
+            cursor = conn.cursor()
+            cursor.execute("select DISTINCT band from contacts")
+            return cursor.fetchall()
+
+    def fetch_all_contacts_asc(self) -> tuple:
+        """returns a tuple of all contacts in the database."""
+        with sqlite3.connect(self.database) as conn:
+            cursor = conn.cursor()
+            cursor.execute("select * from contacts order by date_time ASC")
+            return cursor.fetchall()
+
+    def fetch_all_contacts_desc(self) -> tuple:
+        """returns a tuple of all contacts in the database."""
+        with sqlite3.connect(self.database) as conn:
+            cursor = conn.cursor()
+            cursor.execute("select * from contacts order by date_time desc")
+            return cursor.fetchall()
+
+    def fetch_last_contact(self) -> tuple:
+        """returns a tuple of all contacts in the database."""
+        with sqlite3.connect(self.database) as conn:
+            cursor = conn.cursor()
+            cursor.execute("select * from contacts order by id desc")
+            return cursor.fetchone()
+
+    def dup_check(self, acall: str) -> tuple:
+        """returns a list of possible dups"""
+        with sqlite3.connect(self.database) as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                "select callsign, class, section, band, mode "
+                f"from contacts where callsign like '{acall}' order by band"
+            )
+            return cursor.fetchall()
+
+    def sections(self) -> tuple:
+        """returns a list of sections worked."""
+        with sqlite3.connect(self.database) as conn:
+            cursor = conn.cursor()
+            cursor.execute("select distinct section from contacts")
+            return cursor.fetchall()
+
+    def contact_by_id(self, record) -> tuple:
+        """returns a contact matching an id"""
+        with sqlite3.connect(self.database) as conn:
+            cursor = conn.cursor()
+            cursor.execute("select * from contacts where id=" + record)
+            return cursor.fetchall()
+
+    def get_grids(self) -> tuple:
+        """returns a tuple of unique grids in the log."""
+        with sqlite3.connect(self.database) as conn:
+            cursor = conn.cursor()
+            cursor.execute("select DISTINCT grid from contacts")
+            return cursor.fetchall()
---
+++ server.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+"""This is the description"""
+
+import logging
+import socket
+import time
+import threading
+
+from json import JSONDecodeError, dumps, loads
+from pathlib import Path
+from lib.server_database import DataBase
+from lib.version import __version__
+
+# from lib.settings import Settings
+# pylint: disable=no-name-in-module
+# pylint: disable=c-extension-no-member
+
+
+if Path("./debug").exists():
+    logging.basicConfig(
+        filename="server_debug.log",
+        filemode="w+",
+        format=(
+            "[%(asctime)s] %(levelname)s %(module)s - "
+            "%(funcName)s Line %(lineno)d:\n%(message)s"
+        ),
+        datefmt="%H:%M:%S",
+        level=logging.DEBUG,
+    )
+    logging.debug("Debug started")
+else:
+    logging.basicConfig(level=logging.CRITICAL)
+
+
+def send_pulse():
+    """send heartbeat"""
+    while True:
+        pulse = b'{\n    "cmd": "PING",\n    "host": "server"\n}'
+        s.sendto(pulse, (MULTICAST_GROUP, MULTICAST_PORT))
+        time.sleep(1)
+
+
+DB = DataBase("server_database.db")
+
+MULTICAST_PORT = 2237
+MULTICAST_GROUP = "224.1.1.1"
+INTERFACE_IP = "0.0.0.0"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(("", MULTICAST_PORT))
+mreq = socket.inet_aton(MULTICAST_GROUP) + socket.inet_aton(INTERFACE_IP)
+s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, bytes(mreq))
+
+
+_heartbeat = threading.Thread(
+    target=send_pulse,
+    daemon=True,
+)
+_heartbeat.start()
+
+people = {}
+
+while 1:
+    try:
+        payload = s.recv(1500)
+        if payload == b"Alive":
+            continue
+        json_data = loads(payload.decode())
+        timestamp = time.time()
+
+        if json_data.get("cmd") == "POST":
+
+            print(f"[{timestamp}] {json_data}\n")
+
+            DB.log_contact(
+                (
+                    json_data.get("unique_id"),
+                    json_data.get("hiscall"),
+                    json_data.get("class"),
+                    json_data.get("section"),
+                    json_data.get("frequency"),
+                    json_data.get("band"),
+                    json_data.get("mode"),
+                    json_data.get("power"),
+                    json_data.get("grid"),
+                    json_data.get("opname"),
+                    json_data.get("station"),
+                )
+            )
+
+        if json_data.get("cmd") == "GET":
+            print(f"[{timestamp}] {json_data}\n")
+
+        if json_data.get("cmd") == "DELETE":
+            print(f"[{timestamp}] {json_data}\n")
+            DB.delete_contact(json_data.get("unique_id"))
+
+        if json_data.get("cmd") == "UPDATE":
+            print(f"[{timestamp}] {json_data}\n")
+            DB.change_contact(
+                (
+                    json_data.get("hiscall"),
+                    json_data.get("class"),
+                    json_data.get("section"),
+                    json_data.get("date_time"),
+                    json_data.get("band"),
+                    json_data.get("mode"),
+                    json_data.get("power"),
+                    json_data.get("station"),
+                    json_data.get("unique_id"),
+                )
+            )
+
+        if json_data.get("cmd") == "PING":
+            print(f"[{timestamp}] {json_data}\n")
+            if json_data.get("call"):
+                people[
+                    json_data.get("call")
+                ] = f"{json_data.get('band')} {json_data.get('mode')}"
+                print(people)
+
+    except UnicodeDecodeError as err:
+        print(f"Not JSON: {err}\n{payload}\n")
+    except JSONDecodeError as err:
+        print(f"Not JSON: {err}\n{payload}\n")
