ffb8a387dc5f128fa095a593300107272fe868cc
mbridak <michael.bridak@gmail.com>
2022-09-01 14:06:32 -0700

    Screen tweeks.

--- server.py
+++ server.py
@@ -56,7 +56,7 @@ class Trafficlog:
     def add_item(self, item):
         """adds an item to the log and trims the log"""
         self.items.append(item)
-        if len(self.items) > 9:
+        if len(self.items) > 16:
             self.items = self.items[1 : len(self.items)]
 
     def get_log(self):
@@ -74,8 +74,15 @@ OURCALL = "XXXX"
 OURCLASS = "XX"
 OURSECTION = "XXX"
 ALTPOWER = 0
+
+POINTS = 0
+LASTHOUR = 0
+LAST15 = 0
+
 log = Trafficlog()
 THE_SCREEN = curses.initscr()
+LOGWINDOW = curses.newwin(16, 49, 7, 1)
+QUEWINDOW = curses.newwin(9, 26, 7, 52)
 
 height, width = THE_SCREEN.getmaxyx()
 if height < 24 or width < 80:
@@ -101,6 +108,23 @@ try:
         print("-=* No Settings File Using Defaults *=-")
 except IOError as exception:
     logging.critical("%s", exception)
+
+
+def prectangle(win, uly, ulx, lry, lrx):
+    """Draw a rectangle with corners at the provided upper-left
+    and lower-right coordinates.
+    """
+    win.vline(uly + 1, ulx, curses.ACS_VLINE, lry - uly - 1)
+    win.hline(uly, ulx + 1, curses.ACS_HLINE, lrx - ulx - 1)
+    win.hline(lry, ulx + 1, curses.ACS_HLINE, lrx - ulx - 1)
+    win.vline(uly + 1, lrx, curses.ACS_VLINE, lry - uly - 1)
+    win.addch(uly, ulx, curses.ACS_LTEE)  # UL
+    win.addch(uly, lrx, curses.ACS_RTEE)  # UR
+    try:
+        win.addch(lry, lrx, curses.ACS_LRCORNER)  # LR
+    except curses.error:
+        pass
+    win.addch(lry, ulx, curses.ACS_BTEE)  # LL
 
 
 def send_pulse():
@@ -130,12 +154,11 @@ people = {}
 def comm_log():
     """Display recent UDP traffic"""
     try:
-        logwindow = curses.newwin(10, 49, 9, 1)
-        # rectangle(THE_SCREEN, 8,0,20,50)
-        logwindow.clear()
+        LOGWINDOW.clear()
+        # LOGWINDOW.box()
         for display_line, display_item in enumerate(log.get_log()):
-            logwindow.addstr(display_line, 0, display_item)
-        logwindow.refresh()
+            LOGWINDOW.addstr(display_line, 0, display_item)
+        LOGWINDOW.refresh()
     except curses.error as err:
         logging.debug("%s", err)
     bands = ["160", "80", "40", "20", "15", "10", "6", "2"]
@@ -144,28 +167,28 @@ def comm_log():
     if list_o_bands:
         for count in list_o_bands:
             blist.append(count[0])
-    quewindow = curses.newwin(11, 26, 9, 52)
-    quewindow.clear()
-    quewindow.addstr(0, 0, "Band   CW    PH    DI\n")
-    for band in bands:
-        if band in blist:
-            cwt = DB.get_band_mode_tally(band, "CW")
-            dit = DB.get_band_mode_tally(band, "DI")
-            pht = DB.get_band_mode_tally(band, "PH")
-            line = (
-                f"{str(band).rjust(4, ' ')} "
-                f"{str(cwt[0]).rjust(5, ' ')} "
-                f"{str(pht[0]).rjust(5, ' ')} "
-                f"{str(dit[0]).rjust(5, ' ')}\n"
-            )
-            quewindow.addstr(line)
+    QUEWINDOW.clear()
+    # QUEWINDOW.box()
+    QUEWINDOW.addstr(0, 0, "Band   CW    PH    DI")
+    for yline, band in enumerate(bands):
+        cwt = DB.get_band_mode_tally(band, "CW")
+        dit = DB.get_band_mode_tally(band, "DI")
+        pht = DB.get_band_mode_tally(band, "PH")
+        line = (
+            f"{str(band).rjust(4, ' ')} "
+            f"{str(cwt[0]).rjust(5, ' ')} "
+            f"{str(pht[0]).rjust(5, ' ')} "
+            f"{str(dit[0]).rjust(5, ' ')}"
+        )
+        QUEWINDOW.addstr(yline + 1, 0, line)
     THE_SCREEN.refresh()
-    quewindow.refresh()
+    QUEWINDOW.refresh()
 
 
 def main(_):
     """Main loop"""
     os.environ.setdefault("ESCDELAY", "25")
+    curses.curs_set(0)
     curses.set_tabsize(4)
     curses.start_color()
     curses.use_default_colors()
@@ -173,40 +196,52 @@ def main(_):
         curses.init_color(curses.COLOR_MAGENTA, 1000, 640, 0)
         curses.init_color(curses.COLOR_BLACK, 0, 0, 0)
         curses.init_color(curses.COLOR_CYAN, 500, 500, 500)
-        curses.init_pair(1, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-        curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)
-        curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_BLACK)
-        curses.init_pair(5, curses.COLOR_BLUE, curses.COLOR_BLACK)
-        curses.init_pair(6, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-        curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        curses.init_pair(1, curses.COLOR_MAGENTA, -1)
+        curses.init_pair(2, curses.COLOR_RED, -1)
+        curses.init_pair(3, curses.COLOR_CYAN, -1)
+        curses.init_pair(4, curses.COLOR_GREEN, -1)
+        curses.init_pair(5, curses.COLOR_BLUE, -1)
+        curses.init_pair(6, curses.COLOR_YELLOW, -1)
+        curses.init_pair(7, curses.COLOR_WHITE, -1)
+        curses.init_pair(8, curses.COLOR_BLACK, curses.COLOR_MAGENTA)
     curses.noecho()
     curses.cbreak()
+    THE_SCREEN.attron(curses.color_pair(8))
+    line = f"Field Day aggregation server v{__version__}".center(width, " ")
+    THE_SCREEN.addstr(0, 0, line)
     THE_SCREEN.attron(curses.color_pair(1))
     THE_SCREEN.addstr(
-        0,
-        0,
-        f"\t\t\t\t\tField Day aggregation server v{__version__}\n",
-        curses.color_pair(1),
+        1, 0, "   Group information      Network Information            Scoring\n"
     )
-    THE_SCREEN.addstr("   Group information                      Network Information\n")
-    THE_SCREEN.addstr(2, 40, "Multicast Group: ")
+
+    THE_SCREEN.addstr(2, 1, "Call____:")
+    THE_SCREEN.addstr(f" {OURCALL}", curses.color_pair(7))
+    THE_SCREEN.addstr(3, 1, "Class___:")
+    THE_SCREEN.addstr(f" {OURCLASS}", curses.color_pair(7))
+    THE_SCREEN.addstr(4, 1, "Section_:")
+    THE_SCREEN.addstr(f" {OURSECTION}", curses.color_pair(7))
+    THE_SCREEN.addstr(5, 1, "AltPower:")
+    THE_SCREEN.addstr(f" {bool(ALTPOWER)}", curses.color_pair(7))
+
+    THE_SCREEN.addstr(2, 25, "Multicast Group: ")
     THE_SCREEN.addstr(f"{MULTICAST_GROUP}", curses.color_pair(7))
-    THE_SCREEN.addstr(3, 40, "Multicast Port:  ")
+    THE_SCREEN.addstr(3, 25, "Multicast Port_: ")
     THE_SCREEN.addstr(f"{MULTICAST_PORT}", curses.color_pair(7))
-    THE_SCREEN.addstr(4, 40, "Interface IP:    ")
+    THE_SCREEN.addstr(4, 25, "Interface IP___: ")
     THE_SCREEN.addstr(f"{INTERFACE_IP}", curses.color_pair(7))
 
-    THE_SCREEN.addstr(2, 1, "Call:     ")
-    THE_SCREEN.addstr(f"{OURCALL}", curses.color_pair(7))
-    THE_SCREEN.addstr(3, 1, "Class:    ")
-    THE_SCREEN.addstr(f"{OURCLASS}", curses.color_pair(7))
-    THE_SCREEN.addstr(4, 1, "Section:  ")
-    THE_SCREEN.addstr(f"{OURSECTION}", curses.color_pair(7))
-    THE_SCREEN.addstr(5, 1, "AltPower: ")
-    THE_SCREEN.addstr(f"{bool(ALTPOWER)}", curses.color_pair(7))
-    rectangle(THE_SCREEN, 8, 0, 20, 50)
-    rectangle(THE_SCREEN, 8, 51, 20, 79)
+    THE_SCREEN.addstr(2, 55, "Points_____: ")
+    THE_SCREEN.addstr(f"{POINTS}", curses.color_pair(7))
+    THE_SCREEN.addstr(3, 55, "Last Hour__: ")
+    THE_SCREEN.addstr(f"{LASTHOUR}", curses.color_pair(7))
+    THE_SCREEN.addstr(4, 55, "Last 15 Min: ")
+    THE_SCREEN.addstr(f"{LAST15}", curses.color_pair(7))
+
+    rectangle(THE_SCREEN, 6, 0, 23, 50)
+    rectangle(THE_SCREEN, 6, 50, 16, 79)
+
+    prectangle(THE_SCREEN, 16, 50, 23, 79)
+
     THE_SCREEN.refresh()
     while 1:
         try:
