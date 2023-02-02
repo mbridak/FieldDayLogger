c6fe561f48d72724de42e8ea8ed66c79627a290d
mbridak <michael.bridak@gmail.com>
2023-02-02 12:08:22 -0800

    Added n1mm settings.

--- fdlogger/lib/settings.py
+++ fdlogger/lib/settings.py
@@ -3,6 +3,7 @@
 import logging
 import os
 import pkgutil
+from pathlib import Path
 from json import dumps, loads
 from PyQt5 import QtWidgets, uic
 
@@ -13,6 +14,21 @@ class Settings(QtWidgets.QDialog):
     def __init__(self, parent=None):
         """initialize dialog"""
         super().__init__(parent)
+        self.logger = logging.getLogger("__name__")
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            datefmt="%H:%M:%S",
+            fmt="[%(asctime)s] %(levelname)s %(module)s - %(funcName)s Line %(lineno)d:\n%(message)s",
+        )
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+        if Path("./debug").exists():
+            # if True:
+            self.logger.setLevel(logging.DEBUG)
+            print("debugging on")
+        else:
+            self.logger.setLevel(self.logger.warning)
         self.working_path = os.path.dirname(
             pkgutil.get_loader("fdlogger").get_filename()
         )
@@ -26,7 +42,7 @@ class Settings(QtWidgets.QDialog):
         """setup dialog"""
         with open("./fd_preferences.json", "rt", encoding="utf-8") as file_descriptor:
             self.preference = loads(file_descriptor.read())
-            logging.info("reading: %s", self.preference)
+            self.logger.info("reading: %s", self.preference)
             self.useqrz_radioButton.setChecked(bool(self.preference.get("useqrz")))
             self.usehamdb_radioButton.setChecked(bool(self.preference.get("usehamdb")))
             self.usehamqth_radioButton.setChecked(
@@ -110,6 +126,43 @@ class Settings(QtWidgets.QDialog):
                 else ""
             )
 
+            self.send_n1mm_packets.setChecked(
+                bool(self.preference.get("send_n1mm_packets"))
+            )
+            self.n1mm_station_name.setText(
+                self.preference.get("n1mm_station_name")
+                if self.preference.get("n1mm_station_name")
+                else ""
+            )
+            self.n1mm_operator.setText(
+                self.preference.get("n1mm_operator")
+                if self.preference.get("n1mm_operator")
+                else ""
+            )
+            self.n1mm_ip.setText(
+                self.preference.get("n1mm_ip") if self.preference.get("n1mm_ip") else ""
+            )
+            self.n1mm_radioport.setText(
+                str(self.preference.get("n1mm_radioport"))
+                if self.preference.get("n1mm_radioport")
+                else ""
+            )
+            self.n1mm_contactport.setText(
+                str(self.preference.get("n1mm_contactport"))
+                if self.preference.get("n1mm_contactport")
+                else ""
+            )
+            self.n1mm_lookupport.setText(
+                str(self.preference.get("n1mm_lookupport"))
+                if self.preference.get("n1mm_lookupport")
+                else ""
+            )
+            self.n1mm_scoreport.setText(
+                str(self.preference.get("n1mm_scoreport"))
+                if self.preference.get("n1mm_scoreport")
+                else ""
+            )
+
     def save_changes(self):
         """
         Write preferences to json file.
@@ -140,12 +193,22 @@ class Settings(QtWidgets.QDialog):
         self.preference["multicast_group"] = self.multicast_group.text()
         self.preference["multicast_port"] = self.multicast_port.text()
         self.preference["interface_ip"] = self.interface_ip.text()
+
+        self.preference["send_n1mm_packets"] = self.send_n1mm_packets.isChecked()
+        self.preference["n1mm_station_name"] = self.n1mm_station_name.text()
+        self.preference["n1mm_operator"] = self.n1mm_operator.text()
+        self.preference["n1mm_ip"] = self.n1mm_ip.text()
+        self.preference["n1mm_radioport"] = self.n1mm_radioport.text()
+        self.preference["n1mm_contactport"] = self.n1mm_contactport.text()
+        self.preference["n1mm_lookupport"] = self.n1mm_lookupport.text()
+        self.preference["n1mm_scoreport"] = self.n1mm_scoreport.text()
+
         try:
-            logging.info("save_changes:")
+            self.logger.info("save_changes:")
             with open(
                 "./fd_preferences.json", "wt", encoding="utf-8"
             ) as file_descriptor:
                 file_descriptor.write(dumps(self.preference, indent=4))
-                logging.info("writing: %s", self.preference)
+                self.logger.info("writing: %s", self.preference)
         except IOError as exception:
-            logging.critical("save_changes: %s", exception)
+            self.logger.critical("save_changes: %s", exception)
